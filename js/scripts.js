7cee6c2f631424bc9f2f5d1e387ce8ceb7249681
Wade Willequer <Wade.Willequer@gmail.com>
2019-10-15 12:32:38 -0500

    Added Navbar, also added favicon and basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
-(function() {
-    getWeatherInfo();
+(function () {
+    // getWeatherInfo();
+    geocode();
 })();
 
 // funcrtion to connect to the dark sky api and get weather data
-function getWeatherInfo () {
+function getWeatherInfo() {
     //Base-URL/APIKey/Latitute,Longitude
 
     $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp" })
-    .done(function(data) {
-        console.log(data);
-    })
-    .fail(function() {
-        console.log(error);
-    })
-    .always(function() {
-        console.log("Weather call Complete!");
-    })
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function () {
+            console.log(error);
+        })
+        .always(function () {
+            console.log("Weather call Complete!");
+        })
 }
 
 // function to connect to the mapquest geocoding api and get geocoding data
 function geocode() {
-
+    //Base-URL= + APIKey + &location= + Address
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=Washington,DC")
+        .done(function (data) {
+            console.log(data);
+        })
+        .fail(function () {
+            console.log(error);
+        })
+        .always(function () {
+            console.log("Geocode call finished!");
+        })
 }
